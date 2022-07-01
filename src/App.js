9d0e048d942a8c9bd495a9b8e6e1ed9c4259d295
Mike Wittenauer <mikewittenauer@me.com>
2022-07-01 12:39:03 -0400

    Add app bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { getReps, getSenators } from './apiUtil';
 
 function App() {
@@ -25,6 +27,16 @@ function App() {
 
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <h1>Gov Site</h1>
       <label>Who would you like to contact?</label>
       <br />
